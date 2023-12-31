5b75373e97a6c831cee26cdca791ae481fc23179
ramishshah93 <ramishshah93@gmail.com>
2023-12-31 20:44:19 +0100

    header component done

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,52 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, useState, ChangeEvent } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { ITask } from "./Interfaces";
 
-function App() {
+const App: FC = () => {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadLine] = useState<number>(0);
+  const [todoList, setTodoList] = useState<ITask[]>([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    if (e.target.name === "task") {
+      setTask(e.target.value);
+    }
+    setDeadLine(Number(e.target.value));
+  };
+
+  const addTask = (): void => {
+    const newTask = { taskName: task, deadLine: deadline };
+    setTodoList([...todoList, newTask]);
+    setTask("");
+    setDeadLine(0);
+    console.log(todoList);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            value={task}
+            type="text"
+            placeholder="task..."
+            name="task"
+            onChange={handleChange}
+          />
+          <input
+            value={deadline}
+            type="number"
+            name="deadline"
+            placeholder="deadline in days"
+            onChange={handleChange}
+          />
+        </div>
+        <button onClick={addTask}>Add task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
-}
+};
 
 export default App;
